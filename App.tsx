c498d38910afb4a16c7ea76de2c85f16e1ed7ca8
Juwon Jeong <2jw0718@gmail.com>
2024-10-22 20:45:49 +0900

    타입과 상태 추가

--- App.tsx
+++ App.tsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import { SafeAreaView, StyleSheet, View } from "react-native";
 import Container from "./Container";
 import Item from "./Item";
 
+type FlexDirection = "row" | "column" | "row-reverse" | "column-reverse";
+type JustifyContent =
+  | "flex-start"
+  | "flex-end"
+  | "center"
+  | "space-between"
+  | "space-around"
+  | "space-evenly";
+type AlignItems = "flex-start" | "flex-end" | "center" | "stretch" | "baseline";
+
 export default function App() {
+  const [flexDirection, setFlexDirection] = useState<FlexDirection>("row");
+  const [justifyContent, setJustifyContent] =
+    useState<JustifyContent>("flex-start");
+  const [alignItems, setAlignItems] = useState<AlignItems>("flex-start");
+
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <View style={styles.rootContainer}>
         <Container
           style={{
-            flexDirection: "row",
-            justifyContent: "flex-end",
-            alignItems: "flex-end",
+            flexDirection,
+            justifyContent,
+            alignItems,
           }}
         >
           <Item name="Item 1" />
