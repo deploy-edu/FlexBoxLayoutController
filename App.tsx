2ea7a42c2168303c696c9268cc70ee618d0fd001
Juwon Jeong <2jw0718@gmail.com>
2024-10-22 20:34:45 +0900

    컨테이너 컴포넌트 추가

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import Container from "./Container";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.rootContainer}>
+        <Container></Container>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreaView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  rootContainer: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
   },
 });
---
+++ Container.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+import { StyleSheet, View, ViewStyle } from "react-native";
+
+type Props = {
+  style?: ViewStyle;
+  children?: React.ReactNode;
+};
+
+const Container: FC<Props> = ({ style, children }) => {
+  return <View style={[styles.container, style]}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderWidth: 1,
+    backgroundColor: "#fff",
+  },
+});
+
+export default Container;
